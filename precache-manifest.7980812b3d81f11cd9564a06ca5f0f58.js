97df11f90e55a752d33e015353505d681427f70c
saf <bluesara24@gmail.com>
2023-09-20 20:02:28 -0700

    Updates

---
+++ precache-manifest.7980812b3d81f11cd9564a06ca5f0f58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e085e450562d548fe4a",
+    "url": "/tswift-album-ranker/static/css/main.4b72396b.chunk.css"
+  },
+  {
+    "revision": "8e085e450562d548fe4a",
+    "url": "/tswift-album-ranker/static/js/main.8e085e45.chunk.js"
+  },
+  {
+    "revision": "96f9bec53c751c498073",
+    "url": "/tswift-album-ranker/static/js/1.96f9bec5.chunk.js"
+  },
+  {
+    "revision": "1c16da879b5ff6496427",
+    "url": "/tswift-album-ranker/static/js/runtime~main.1c16da87.js"
+  },
+  {
+    "revision": "4d61ead76acfdab56f84c12f2ae80caa",
+    "url": "/tswift-album-ranker/index.html"
+  }
+];
