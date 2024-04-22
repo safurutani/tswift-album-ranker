010dfd4e4f940c1d312e1581685ca6fb87834877
safurutani <bluesara24@gmail.com>
2024-04-21 20:05:30 -0700

    Updates

--- precache-manifest.cbd0dc84b4699c7783e64d367651d57d.js
+++ precache-manifest.cbd0dc84b4699c7783e64d367651d57d.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "95d10b7ec153e4adc1a9",
-    "url": "/tswift-album-ranker/static/css/main.5be9c3c7.chunk.css"
+    "revision": "7d3f6df24cd1fb7d49d5",
+    "url": "/tswift-album-ranker/static/css/main.730bf085.chunk.css"
   },
   {
-    "revision": "95d10b7ec153e4adc1a9",
-    "url": "/tswift-album-ranker/static/js/main.95d10b7e.chunk.js"
+    "revision": "7d3f6df24cd1fb7d49d5",
+    "url": "/tswift-album-ranker/static/js/main.7d3f6df2.chunk.js"
   },
   {
     "revision": "96f9bec53c751c498073",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/tswift-album-ranker/static/js/runtime~main.1c16da87.js"
   },
   {
-    "revision": "bb18844abfc23101710e2a143685c4b0",
+    "revision": "e484c05ddac5b5bfbb6af28fed048def",
     "url": "/tswift-album-ranker/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tswift-album-ranker/precache-manifest.854e123c3b448215e754ecbf39b4784c.js"
+  "/tswift-album-ranker/precache-manifest.cbd0dc84b4699c7783e64d367651d57d.js"
 );
 
 workbox.clientsClaim();
